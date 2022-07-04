24e694eeb3a97e3d6e466b45732958b0ec4fbe05
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2022-07-04 11:00:15 +0530

    Add files via upload

---
+++ Array/Basic.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num[10];
+
+    //Accessing an is_array 
+    cout << "Value at index 10 is " << num[10] <<endl;
+
+    //initialising an array
+    int num2[3]={12,23,34};
+
+    //accessing an element
+    cout << "value at index 2 is " << num2[2] << endl;
+
+    int num3[15]={11,22};
+
+    int n = 3;
+
+    cout << " Printing an array " << endl;
+
+    //print the array
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << num3[i] <<" ";
+
+
+
+
+    }
+    
+
+
+
+}
