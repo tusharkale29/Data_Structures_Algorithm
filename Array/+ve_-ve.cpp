bd0c9802ee74f8e166b85555b701bf292022fb95
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2022-07-11 16:44:07 +0530

    Add files via upload

---
+++ Array/+ve_-ve.cpp
@@ -0,0 +1,26 @@
+// Question : To find positive and negative number in given array : 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+    int num;
+     cout << "Enter the number to check " << endl;
+     cin >> num;
+
+     if (num > 0)
+     {
+         cout <<  "Number is Positive number " << endl;
+    }
+     else
+    {
+         cout << "Number is Negative" << endl;
+     }
+
+    return 0;
+
+}
