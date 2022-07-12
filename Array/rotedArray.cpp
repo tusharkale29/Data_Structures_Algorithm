9ee20746a72e7b378d1ea3bd7cbf46e73d9ab749
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2022-07-12 11:07:48 +0530

    Add files via upload

---
+++ Array/rotedArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[6] = {1,2,3,4,5,6};
+
+    
+    //  for (i=1; i<6; i++)
+    // {
+    //      cout << arr[6];
+    
+    // }
+    do
+    {
+        swap (arr[0], arr[6-1]);
+        /* code */
+    } while (arr[i]<3);
+    
+
+
+
+}
