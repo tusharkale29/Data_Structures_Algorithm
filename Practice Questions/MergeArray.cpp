26605560fed4aacc1cb018e6e5aa3f9fc1902597
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2022-08-02 01:04:00 +0530

    Add files via upload

---
+++ Practice Questions/MergeArray.cpp
@@ -0,0 +1,69 @@
+// program to merge two arrays :
+
+// created on : 01-08-2022 : 
+
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int A[10]; 
+    int size;
+    int length;
+};
+
+void display(struct Array arr)
+{
+    int i;
+    cout << endl << "Elements are : "<<endl;
+    for (i=0; i<arr.length; i++) 
+    {
+        cout<<arr.A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+struct Array* Merge(struct Array *arr1, struct Array *arr2 )
+{
+    int i,j,k;
+    i=j=k=0;
+    
+    struct Array *arr3 = new struct Array;
+
+    while (i<arr1->length && j<arr2->length)
+     {
+        if (arr1->A[i] < arr2->A[j]) 
+        {
+            arr3->A[k++] = arr1->A[i++];
+        } 
+        else
+         {
+            arr3->A[k++] = arr2->A[j++];
+        }
+    }
+    
+    for (; i<arr1->length; i++) 
+    {
+        arr3->A[k++] = arr1->A[i];
+    }
+    for (; j<arr2->length; j++) 
+    {
+        arr3->A[k++] = arr2->A[j];
+    }
+    
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+    
+    return arr3;
+
+}
+
+int main() 
+{
+    struct Array arr1={{3,8,16,20,25},10,5};
+    struct Array arr2={{4,10,12,22,23},10,5};
+    struct Array *arr3;
+    arr3 = Merge(&arr1, &arr2);
+    display(*arr3);
+    return 0;
+}
